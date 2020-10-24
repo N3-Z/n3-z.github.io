3aa51f995b10744aece7b96d9098ed3a1e6236c1
N3-Z <xavani.ao@gmail.com>
2020-10-25 02:12:07 +0700

    first commit new

---
+++ precache-manifest.354ece86896c5fd8f03762ba9d06fdbc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b60712d7cc3af4b9c330f3213b5ebd79",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2a0873c3c70b5c51ec4a",
+    "url": "./static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "09fec1bf75743f8f8bf5",
+    "url": "./static/css/main.e3b9ec1e.chunk.css"
+  },
+  {
+    "revision": "2a0873c3c70b5c51ec4a",
+    "url": "./static/js/2.c94e51f4.chunk.js"
+  },
+  {
+    "revision": "7c6e484c775d69d82558a4b25a5b8dad",
+    "url": "./static/js/2.c94e51f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09fec1bf75743f8f8bf5",
+    "url": "./static/js/main.ac9198c8.chunk.js"
+  },
+  {
+    "revision": "2107c41fb5c2ea8f55f0",
+    "url": "./static/js/runtime-main.99249458.js"
+  },
+  {
+    "revision": "a5ebdbb1944f9dacffbb9a10de52890b",
+    "url": "./static/media/1.a5ebdbb1.jpg"
+  },
+  {
+    "revision": "09c3ee5b1885cf15421695626be43320",
+    "url": "./static/media/IMG_1131.09c3ee5b.JPG"
+  },
+  {
+    "revision": "f278be7973516129af9e64d45a0c6f4d",
+    "url": "./static/media/c++.f278be79.png"
+  },
+  {
+    "revision": "2097a2c1f2425ce3ae54c37f47c91f2c",
+    "url": "./static/media/image2.2097a2c1.webp"
+  },
+  {
+    "revision": "09ff82021d2ceccba8c8e8c2a553ca48",
+    "url": "./static/media/image3.09ff8202.webp"
+  },
+  {
+    "revision": "9618579e1da207558cb69e4921dcae29",
+    "url": "./static/media/image4.9618579e.webp"
+  },
+  {
+    "revision": "0f26e1c07558005aba4c71d56332d6f9",
+    "url": "./static/media/java.0f26e1c0.png"
+  },
+  {
+    "revision": "64b15e574ec8cc1f3c0ed8b0db5dbcad",
+    "url": "./static/media/linux.64b15e57.png"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "./static/media/react.580ffd26.png"
+  },
+  {
+    "revision": "6602796da2d3b57180cc301d09b2614e",
+    "url": "./static/media/slider2.6602796d.PNG"
+  }
+]);
